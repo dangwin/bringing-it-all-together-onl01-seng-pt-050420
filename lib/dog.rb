0bd4810368093c9a145778d2b6612adb8dcf68b9
Dang Nguyen <danghwin@gmail.com>
2020-07-12 22:11:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,12 +20,11 @@ class Dog
       end
       
       def self.drop_table
-        sql = <<-SQL 
-        DROP TABLE dogs 
-        SQL 
-        
-         DB[:conn].execute(sql)
+         sql = <<-SQL
+          DROP TABLE dogs
+          SQL
+    
+        DB[:conn].execute(sql)
       end
-        
   
 end 
