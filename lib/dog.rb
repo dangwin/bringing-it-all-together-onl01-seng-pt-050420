aae73a391f331a6f6225f9942a9bc19b539c67ee
Dang Nguyen <danghwin@gmail.com>
2020-07-12 22:07:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,4 +2,9 @@ class Dog
   
   attr_accessor :id, :name, :breed 
   
+  def initialize(attributes)
+        attributes.each {|key, value| self.send(("#{key}="), value)}
+        self.id ||= nil
+    end
+  
 end 
